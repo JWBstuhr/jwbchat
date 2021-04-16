9a4a9460e14d056ece9ee86befea055d4644af99
JWBstuhr <77174240+JWBstuhr@users.noreply.github.com>
2021-04-15 21:30:01 -0500

    Add files via upload

---
+++ jcupdater.lua
@@ -0,0 +1,7 @@
+print("You'll have to set your Username, Channel, and Audio again!")
+sleep(2)
+shell.run("rm " .. shell.getRunningProgram())
+shell.run("rm jwbchat.lua")
+shell.run("rm /jc/jconfig")
+shell.run("wget https://github.com/JWBstuhr/jwbchat/raw/main/jwbchat.lua")
+shell.run("jwbchat.lua")
